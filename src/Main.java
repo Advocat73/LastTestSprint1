d2a2edd87e7e330b7f39d13baba3b15bab8abea2
Advocat73 <toropov_alex@mail.ru>
2023-07-24 22:38:07 +0200

    Last version

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        Transport transport[] = new Transport[2];
+       System.out.println("Hello and welcome!");
+        Transport transport[] = new Transport[3];
         transport[0] = new Transport("Red", 120);
         transport[1] = new Transport("Green", 160);
+        transport[1] = new Transport("Black", 200);
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i < 3; i++) {
+        for (int i = 1; i < 4; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
